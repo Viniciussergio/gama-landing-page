ab610cc7d6be865a797883b1d6f7f414782bedc7
Vinicius Sergio Silva de Oliveira <viniciussergio.so@gmail.com>
2021-07-17 18:12:57 -0300

    Valeu edison

--- gama-landing-page/src/App.js
+++ gama-landing-page/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Valeu Edison salvou minha pele cara
         </p>
         <a
           className="App-link"
